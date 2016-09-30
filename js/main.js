09c7275f7bfd56995643ee3c7b012aeb330630f9
Ruben AvilaH <rubenavilah@homecs.co>
2016-09-29 22:52:40 -0500

    Initial setup Calculator

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nombre = prompt("Primero me gustaria saber tu nombre");
+var Bienvenida = prompt("Bienvenido a la calculadora " + nombre);
+var number_one = prompt("Ingresa un número");
+var number_two = prompt("Ingresa otro número");
+var operator = prompt("Ingresa un operador");
