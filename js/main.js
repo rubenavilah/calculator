3d76173e58c06af7552a6d0d9fe6c60fc723113b
Ruben AvilaH <rubenavilah@homecs.co>
2016-09-30 00:05:41 -0500

    Addition and Substraction simple

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-var nombre = prompt("Primero me gustaria saber tu nombre");
-var Bienvenida = prompt("Bienvenido a la calculadora " + nombre);
-var number_one = prompt("Ingresa un número");
-var number_two = prompt("Ingresa otro número");
+// Variables of the calculator
+// var nombre = prompt("Primero me gustaria saber tu nombre");
+// var bienvenida = alert("Bienvenido a la calculadora " + nombre);
+var data_one = prompt("Ingresa un número");
+var data_two = prompt("Ingresa otro número");
 var operator = prompt("Ingresa un operador");
+
+//prompts to number
+var number_one = parseFloat(data_one);
+var number_two = parseFloat(data_two);
+
+//Sum
+var sum = function(){
+  result = number_one + number_two;
+  return result;
+};
+
+//Substraction
+var subs = function(){
+  result = number_one - number_two;
+  return result;
+};
+
+// The print
+var print = function(data){
+  console.log(data);
+};
+
+//The calls
+if (operator === "suma"){
+  sum(number_one, number_two);
+  print(result);
+} else if (operator === "resta") {
+  subs(number_one, number_two);
+  print(result);
+}
