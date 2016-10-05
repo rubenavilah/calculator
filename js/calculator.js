90ed5f776f0836d12afbe6793744d8355e82faee
Ruben AvilaH <rubenavilah@gmail.com>
2016-10-04 21:16:30 -0500

    Add yhe numbers to Input

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,15 @@
-calculator = {
+var calculator = {
   op1 : null,
   op2: null,
   operador: null,
   check_input: function(){
+    console.log("Check input");
     var input = document.getElementById("input");
     var input_text = input.textContent;
     var input_number = Number(input_text);
     var errors = document.querySelector("#error");
-    if (input_number !== NaN) {
+    if (!Number.isNaN(input_number)) {
+      console.log("Is a number");
       if (this.opt1 === null) {
           this.op1 = input_number;
       }else{
@@ -17,13 +19,28 @@ calculator = {
       console.log("Error");
     }
   },
+  set_number: function(button){
+    return function(){
+      var input = document.getElementById("input");
+      input.value = "" + input.value + button.textContent;
+      console.log(this);
+    }; // return
+  }, // set_number
 };
 document.addEventListener("DOMContentLoaded", function(event){
+  // Once the Page loads
   var output = document.getElementById("result");
   var controlpad = document.getElementById("controlpad");
-  var button_operators = document.querySelectorAll("#operators");
-  var i = 0;
+  var button_operators = controlpad.querySelectorAll("#operators button");
+  var i;
   for (i = 0; i < button_operators.length; i++) {
-    button_operators[i].addEventListener("click", calculator.check_input());
+    button_operators[i].addEventListener("click", calculator.check_input);
   }
+  var button_numbers = controlpad.querySelectorAll("#numbers button");
+  for (i = 0; i < button_numbers.length; i++) {
+    button_numbers[i].addEventListener(
+      "click",
+      calculator.set_number(button_numbers[i]));
+  }//addEventListener
+  console.log("Initiated");
 });
